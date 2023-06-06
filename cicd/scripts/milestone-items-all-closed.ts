43206643155bf6422a1cc58dae2881b720d111c2
Calvin Wilkinson <85414302+CalvinWilkinson@users.noreply.github.com>
2023-06-06 16:26:49 +0100

    🚧Remove script description code (#30)
    * Start work for issue #29
    
    * chore: remove all ScriptDescription usages in scripts

--- cicd/scripts/milestone-items-all-closed.ts
+++ cicd/scripts/milestone-items-all-closed.ts
@@ -2,12 +2,9 @@ import { MilestoneClient } from "../core/MilestoneClient.ts";
 import { IIssueModel } from "../core/Models/IIssueModel.ts";
 import { IPullRequestModel } from "../core/Models/IPullRequestModel.ts";
 import { RepoClient } from "../core/RepoClient.ts";
-import { ScriptDescriptions } from "../core/ScriptDescriptions.ts";
 import { Utils } from "../core/Utils.ts";
 
 const scriptName = Utils.getScriptName();
-const scriptDescriptions: ScriptDescriptions = new ScriptDescriptions();
-scriptDescriptions.printScriptDescription(scriptName);
 
 if (Deno.args.length < 3) {
 	let errorMsg =
