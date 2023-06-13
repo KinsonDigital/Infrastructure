a5fbae050db013adeb1166132e227420bba84bed
Calvin Wilkinson <kinsondigital@gmail.com>
2023-06-13 10:51:34 +0100

    misc: create client that is specific to github

---
+++ cicd/core/GitHubClient.ts
@@ -0,0 +1,35 @@
+import { RESTClient } from "./RESTClient.ts";
+import { Utils } from "./Utils.ts";
+
+/**
+ * Provides a base class for HTTP clients.
+ */
+export abstract class GitHubClient extends RESTClient {
+	protected readonly organization = "KinsonDigital";
+	protected readonly headers: Headers = new Headers();
+	
+	/**
+	 * Initializes a new instance of the {@link RESTClient} class.
+	 * @param token The GitHub token to use for authentication.
+	 * @remarks If no token is provided, then the client will not be authenticated.
+	*/
+	constructor(token?: string) {
+		super();
+
+		this.baseUrl = "https://api.github.com/repos";
+		this.headers.append("Accept", "application/vnd.github.v3+.json");
+		this.headers.append("X-GitHub-Api-Version", "2022-11-28");
+
+		if (!Utils.isNullOrEmptyOrUndefined(token)) {
+			this.headers.append("Authorization", `Bearer ${token}`);
+		}
+	}
+
+	/**
+	 * Returns a value indicating whether or not a token was provided.
+	 * @returns True if a token was provided; otherwise, false.
+	 */
+	protected containsToken(): boolean {
+		return this.headers.has("Authorization");
+	}
+}
