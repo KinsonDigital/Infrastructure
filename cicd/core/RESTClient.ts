378de0c1412e99871a4a908b0cbf24055c7c7d5a
Calvin Wilkinson <kinsondigital@gmail.com>
2023-06-13 10:52:49 +0100

    refactor: remove github related code from the rest client

--- cicd/core/RESTClient.ts
+++ cicd/core/RESTClient.ts
@@ -1,34 +1,11 @@
-import { Utils } from "./Utils.ts";
+import { Guard } from "./Guard.ts";
 
 /**
  * Provides a base class for HTTP clients.
  */
 export abstract class RESTClient {
-	protected readonly organization = "KinsonDigital";
-	protected baseUrl = "https://api.github.com/repos";
 	protected readonly headers: Headers = new Headers();
-
-	/**
-	 * Initializes a new instance of the {@link RESTClient} class.
-	 * @param token The GitHub token to use for authentication.
-	 * @remarks If no token is provided, then the client will not be authenticated.
-	 */
-	constructor(token?: string) {
-		this.headers.append("Accept", "application/vnd.github.v3+.json");
-		this.headers.append("X-GitHub-Api-Version", "2022-11-28");
-
-		if (!Utils.isNullOrEmptyOrUndefined(token)) {
-			this.headers.append("Authorization", `Bearer ${token}`);
-		}
-	}
-
-	/**
-	 * Returns a value indicating whether or not a token was provided.
-	 * @returns True if a token was provided; otherwise, false.
-	 */
-	protected containsToken(): boolean {
-		return this.headers.has("Authorization");
-	}
+	protected baseUrl = "";
 
 	/**
 	 * Gets the data from an HTTP response.
@@ -47,6 +24,8 @@ export abstract class RESTClient {
 	 * @returns The response from the fetch request.
 	 */
 	protected async fetchGET(url: string): Promise<Response> {
+		Guard.isNullOrEmptyOrUndefined(url, "fetchGET", "url");
+
 		return await fetch(url, {
 			method: "GET",
 			headers: this.headers,
@@ -60,6 +39,8 @@ export abstract class RESTClient {
 	 * @returns The response from the fetch request.
 	 */
 	protected async fetchPATCH(url: string, body: string): Promise<Response> {
+		Guard.isNullOrEmptyOrUndefined(url, "fetchGET", "url");
+
 		return await fetch(url, {
 			method: "PATCH",
 			headers: this.headers,
