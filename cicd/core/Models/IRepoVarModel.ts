9fc4408497c3fb8189bb32d7dff0f5107607d81e
Calvin Wilkinson <kinsondigital@gmail.com>
2023-06-27 22:45:49 +0100

    chore: create a model to hold repo variable data

---
+++ cicd/core/Models/IRepoVarModel.ts
@@ -0,0 +1,24 @@
+/**
+ * Represents a single repository variable.
+ */
+export interface IRepoVarModel {
+	/**
+	 * Gets or sets the name of the variable.
+	 */
+	name: string;
+
+	/**
+	 * Gets or sets the value of the variable.
+	 */
+	value: string;
+
+	/**
+	 * Gets or sets the created date of the variable.
+	 */
+	created_at: string;
+
+	/**
+	 * Gets or sets the updated date of the variable.
+	 */
+	updated_at: string;
+}
