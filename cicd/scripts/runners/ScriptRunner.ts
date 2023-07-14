bcf755645aeceebd694449d5674c26eeb0c5b79d
Calvin Wilkinson <kinsondigital@gmail.com>
2023-07-14 15:00:15 +0100

    refactor: move release type and version arg validation to base class

--- cicd/scripts/runners/ScriptRunner.ts
+++ cicd/scripts/runners/ScriptRunner.ts
@@ -1,3 +1,5 @@
+import { Utils } from "../../core/Utils.ts";
+
 /**
  * Provides a base class for processing script arguments and running scripts.
  */
@@ -23,5 +25,38 @@ export abstract class ScriptRunner {
 	 * Runs a script.
 	 */
 	protected async run(): Promise<void> {
+	}
+
+	/**
+	 * Validates the given {@link version} and {@link releaseType}.
+	 * @param releaseType The type of release to validate.
+	 * @param version The version to validate.
+	 */
+	protected validateVersionAndReleaseType(releaseType: string, version: string): void {
+		releaseType = releaseType.trim().toLowerCase();
+		version = version.trim().toLowerCase();
+
+		version = version.startsWith("v") ? version : `v${version}`;
+
+		if (releaseType != "production" && releaseType != "preview") {
+			Utils.printAsGitHubError(`The release type must be either 'production' or 'preview'.`);
+			Deno.exit(1);
+		}
+
+		if (releaseType == "production") {
+			if (Utils.isNotValidProdVersion(version)) {
+				let errorMsg = `The production version '${version}' is not valid.`;
+				errorMsg += "\nRequired Syntax: v#.#.#";
+				Utils.printAsGitHubError(errorMsg);
+				Deno.exit(1);
+			}
+		} else {
+			if (Utils.isNotValidPreviewVersion(version)) {
+				let errorMsg = `The preview version '${version}' is not valid.`;
+				errorMsg += "\nRequired Syntax: v#.#.#-preview.#";
+				Utils.printAsGitHubError(errorMsg);
+				Deno.exit(1);
+			}
+		}
 	}
 }
