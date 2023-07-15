46c0bbdcbfd9380aaa14c92c6e79bf2408c45564
Calvin Wilkinson <kinsondigital@gmail.com>
2023-07-15 14:17:01 +0100

    cleanup: remove script and related code for validating release notes

--- cicd/scripts/generate-release-notes.ts
+++
@@ -1,8 +0,0 @@
-import { Utils } from "../core/Utils.ts";
-import { GenerateReleaseNotesRunner } from "./runners/GenerateReleaseNotesRunner.ts";
-
-const scriptName = Utils.getScriptName();
-
-const runner: GenerateReleaseNotesRunner = new GenerateReleaseNotesRunner(Deno.args, scriptName);
-
-await runner.run();
