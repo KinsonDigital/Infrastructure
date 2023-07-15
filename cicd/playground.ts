2d4d07a13b9da8a0319b7f2debb3af2ff6e75b4f
Calvin Wilkinson <kinsondigital@gmail.com>
2023-07-15 15:58:23 +0100

    chore: set new default for playground script

--- cicd/playground.ts
+++ cicd/playground.ts
@@ -1,5 +1,2 @@
 const _token = Deno.args[0]; // NOTE: This is coming from the launch.config json file as an environment variable
 const _rootRepoDirPath = Deno.args[1];
-
-const token = Deno.args[0]; // NOTE: This is coming from the launch.config json file as an environment variable
-const rootRepoDirPath = Deno.args[1];
