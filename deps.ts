02edace6c03aef3aaaa101df3c377b691303fd5b
Calvin Wilkinson <kinsondigital@gmail.com>
2023-11-19 12:24:13 +0000

    deps: setup deps.ts file with imports and exports

---
+++ deps.ts
@@ -0,0 +1,10 @@
+import {
+	IssueClient, PullRequestClient, ProjectClient,
+	OrgClient, RepoClient
+} from "https://deno.land/x/kd_clients@v1.0.0-preview.4/GitHubClients/mod.ts";
+
+
+export {
+	IssueClient, PullRequestClient, ProjectClient,
+	OrgClient, RepoClient
+}
