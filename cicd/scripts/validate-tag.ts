4a6c06e80f40ec94712d137cd78d9d88d07f9894
Calvin Wilkinson <kinsondigital@gmail.com>
2023-11-19 16:12:04 +0000

    refactor: update validate tag script to use kd_clients

--- cicd/scripts/validate-tag.ts
+++ cicd/scripts/validate-tag.ts
@@ -1,27 +1,29 @@
-import { RepoClient } from "../clients/RepoClient.ts";
-import { TagClient } from "../clients/TagClient.ts";
+import { RepoClient, TagClient } from "../../deps.ts";
 import { Utils } from "../core/Utils.ts";
 
 // Validate the arguments
-if (Deno.args.length != 4) {
+if (Deno.args.length != 5) {
 	let errorMsg = `The cicd script must have 4 arguments but has ${Deno.args.length} argument(s).`;
-	errorMsg += "\nThe 1st arg is required and must be a valid GitHub repo.";
-	errorMsg += "\nThe 2nd arg is required and must be either 'production', 'preview' or 'either'.";
-	errorMsg += "\nThe 3rd arg is required and must be the name of the tag.";
-	errorMsg += "\nThe 4th arg is required and must be a GitHub PAT (Personal Access Token).";
+	errorMsg += "\nThe 1st arg is required and must be a valid GitHub repository owner name.";
+	errorMsg += "\nThe 2nd arg is required and must be a valid GitHub repo.";
+	errorMsg += "\nThe 3rd arg is required and must be either 'production', 'preview' or 'either'.";
+	errorMsg += "\nThe 4th arg is required and must be the name of the tag.";
+	errorMsg += "\nThe 5th arg is required and must be a GitHub PAT (Personal Access Token).";
 
 	Utils.printAsGitHubError(errorMsg);
 	Deno.exit(1);
 }
 
-const repoName: string = Deno.args[0];
-const tagType: string = Deno.args[1].toLowerCase();
-let tag: string = Deno.args[2].trim();
-tag = tag.startsWith("v") ? tag : `v${Deno.args[2]}`;
-const token = Deno.args.length >= 4 ? Deno.args[3].trim() : "";
+const ownerName: string = Deno.args[0];
+const repoName: string = Deno.args[1];
+const tagType: string = Deno.args[2].toLowerCase();
+let tag: string = Deno.args[3].trim();
+tag = tag.startsWith("v") ? tag : `v${Deno.args[3]}`;
+const token = Deno.args.length >= 5 ? Deno.args[4].trim() : "";
 
 // Print out all of the arguments
 Utils.printInGroup("Script Arguments", [
+	`Owner Name (Required): ${ownerName}`,
 	`Repo Name (Required): ${repoName}`,
 	`Tag Type (Required): ${tagType}`,
 	`Tag (Required): ${tag}`,
@@ -60,17 +62,17 @@ if (tagIsInvalid) {
 	Deno.exit(1);
 }
 
-const repoClient: RepoClient = new RepoClient(token);
-const repoDoesNotExist = !(await repoClient.exists(repoName));
+const repoClient: RepoClient = new RepoClient(ownerName, repoName, token);
+const repoDoesNotExist = !(await repoClient.exists());
 
 if (repoDoesNotExist) {
 	Utils.printAsGitHubError(`The repository '${repoName}' does not exist.`);
 	Deno.exit(1);
 }
 
-const tagClient: TagClient = new TagClient(token);
+const tagClient: TagClient = new TagClient(ownerName, repoName, token);
 
-const tagExists = await tagClient.tagExists(repoName, tag);
+const tagExists = await tagClient.tagExists(tag);
 
 if (tagExists) {
 	Utils.printAsGitHubError(`The tag '${tag}' already exists.`);
