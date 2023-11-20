c65876eb0aa89daa9428b891ecb322311380ed11
Calvin Wilkinson <kinsondigital@gmail.com>
2023-11-20 10:37:29 +0000

    cleanup: remove unused types

--- cicd/core/Types.ts
+++ cicd/core/Types.ts
@@ -9,28 +9,3 @@ export type ItemType = "issue" | "pull-request";
  * Represents a GitHub issue or pull request.
  */
 export type IssueOrPR = IssueModel | PullRequestModel;
-
-/**
- * Represents any branch.
- */
-export type AnyBranch = null;
-
-/**
- * Represents a function for getting a page of data form a GitHub API end point.
- */
-export type GetDataFunc<T> = (page: number, qtyPerPage?: number) => Promise<[T[], Response]>;
-
-/**
- * Represents a bad credentials object returned from the GitHub GraphQL API.
- */
-export type BadCredentials = {
-	/**
-	 * The documentation URL about the GraphQL API.
-	 */
-	documentation_url: string;
-
-	/**
-	 * The message about the bad credentials.
-	 */
-	message: string;
-};
