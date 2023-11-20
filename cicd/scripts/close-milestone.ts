97f3b3fbe91465c73e6f15794436e3a3afdc98d7
Calvin Wilkinson <kinsondigital@gmail.com>
2023-11-20 14:30:21 +0000

    refactor: refactor scripts to be imported into a single module

--- cicd/scripts/close-milestone.ts
+++ cicd/scripts/close-milestone.ts
@@ -2,40 +2,46 @@ import { RepoClient, MilestoneClient } from "../../deps.ts";
 import { MilestoneModel } from "../../deps.ts";
 import { Utils } from "../core/Utils.ts";
 
-if (Deno.args.length != 3) {
-	let errorMsg = `The cicd script must have 4 arguments but has ${Deno.args.length} argument(s).`;
-	errorMsg += "\nThe 1st arg is required and must be the GitHub name of the owner of the repository.";
-	errorMsg += "\nThe 2st arg is required and must be the GitHub repo name.";
-	errorMsg += "\nThe 3rd arg is required and must be a valid milestone name.";
-	errorMsg += "\nThe 4th arg is required and must be a valid GitHub PAT (Personal Access Token).";
+const closeMilestoneExecutor = async () => {
+	if (Deno.args.length != 3) {
+		let errorMsg = `The cicd script must have 4 arguments but has ${Deno.args.length} argument(s).`;
+		errorMsg += "\nThe 1st arg is required and must be the GitHub name of the owner of the repository.";
+		errorMsg += "\nThe 2st arg is required and must be the GitHub repo name.";
+		errorMsg += "\nThe 3rd arg is required and must be a valid milestone name.";
+		errorMsg += "\nThe 4th arg is required and must be a valid GitHub PAT (Personal Access Token).";
 
-	Utils.printAsGitHubError(errorMsg);
-	Deno.exit(1);
-}
+		Utils.printAsGitHubError(errorMsg);
+		Deno.exit(1);
+	}
+	
+	const ownerName: string = Deno.args[0].trim();
+	const repoName: string = Deno.args[1].trim();
+	const milestoneName: string = Deno.args[2].trim();
+	const token = Deno.args[3].trim();
+	
+	// Print out all of the arguments
+	Utils.printInGroup("Script Arguments", [
+		`Owner Name (Required): ${ownerName}`,
+		`Repo Name (Required): ${repoName}`,
+		`Milestone Name (Required): ${milestoneName}`,
+		`GitHub Token (Required): ****`,
+	]);
+	
+	const repoClient: RepoClient = new RepoClient(ownerName, repoName, token);
+	const repoDoesNotExist = !(await repoClient.exists());
+	
+	if (repoDoesNotExist) {
+		Utils.printAsGitHubError(`The repository '${repoName}' does not exist.`);
+		Deno.exit(1);
+	}
+	
+	const milestoneClient: MilestoneClient = new MilestoneClient(ownerName, repoName, token);
 
-const ownerName: string = Deno.args[0].trim();
-const repoName: string = Deno.args[1].trim();
-const milestoneName: string = Deno.args[2].trim();
-const token = Deno.args[3].trim();
+	const milestone: MilestoneModel = await milestoneClient.getMilestoneByName(milestoneName);
 
-// Print out all of the arguments
-Utils.printInGroup("Script Arguments", [
-	`Owner Name (Required): ${ownerName}`,
-	`Repo Name (Required): ${repoName}`,
-	`Milestone Name (Required): ${milestoneName}`,
-	`GitHub Token (Required): ****`,
-]);
+	await milestoneClient.closeMilestone(milestone.title);
+};
 
-const repoClient: RepoClient = new RepoClient(ownerName, repoName, token);
-const repoDoesNotExist = !(await repoClient.exists());
+closeMilestoneExecutor();
 
-if (repoDoesNotExist) {
-	Utils.printAsGitHubError(`The repository '${repoName}' does not exist.`);
-	Deno.exit(1);
-}
-
-const milestoneClient: MilestoneClient = new MilestoneClient(ownerName, repoName, token);
-
-const milestone: MilestoneModel = await milestoneClient.getMilestoneByName(milestoneName);
-
-await milestoneClient.closeMilestone(milestone.title);
+export default closeMilestoneExecutor;
--- cicd/scripts/label-if-head-branch.ts
+++ cicd/scripts/label-if-head-branch.ts
@@ -5,68 +5,74 @@ import {
 from "../../deps.ts";
 import { Utils } from "../core/Utils.ts";
 
-if (Deno.args.length != 6) {
-	let errorMsg = `The  cicd script must have 7 arguments but has ${Deno.args.length} argument(s).`;
-	errorMsg += "\nThe 1st arg is required and must be the GitHub repository owner name.";
-	errorMsg += "\nThe 2nd arg is required and must be the GitHub repo name.";
-	errorMsg += "\nThe 3rd arg is required and must be a valid pull request number.";
-	errorMsg += "\nThe 4th arg is required and must be the head branch of the pull request.";
-	errorMsg += "\nThe 5th arg is required and must be the intended head branch of the pull request.";
-	errorMsg += "\nThe 6th arg is required and must be the label to add if the head branch of the pull request is correct.";
-	errorMsg += "\nThe 7th arg is required and must be a GitHub PAT (Personal Access Token).";
+const labelIfHeadBranchExecutor = async () => {
+	if (Deno.args.length != 6) {
+		let errorMsg = `The  cicd script must have 7 arguments but has ${Deno.args.length} argument(s).`;
+		errorMsg += "\nThe 1st arg is required and must be the GitHub repository owner name.";
+		errorMsg += "\nThe 2nd arg is required and must be the GitHub repo name.";
+		errorMsg += "\nThe 3rd arg is required and must be a valid pull request number.";
+		errorMsg += "\nThe 4th arg is required and must be the head branch of the pull request.";
+		errorMsg += "\nThe 5th arg is required and must be the intended head branch of the pull request.";
+		errorMsg += "\nThe 6th arg is required and must be the label to add if the head branch of the pull request is correct.";
+		errorMsg += "\nThe 7th arg is required and must be a GitHub PAT (Personal Access Token).";
+		
+		Utils.printAsGitHubError(errorMsg);
+		Deno.exit(1);
+	}
 
-	Utils.printAsGitHubError(errorMsg);
-	Deno.exit(1);
-}
+	const ownerName = Deno.args[0].trim();
+	const repoName = Deno.args[1].trim();
+	let prNumber = 0;
 
-const ownerName = Deno.args[0].trim();
-const repoName = Deno.args[1].trim();
-let prNumber = 0;
+	if (Utils.isNumeric(Deno.args[2].trim())) {
+		prNumber = parseInt(Deno.args[2].trim());
+	} else {
+		Utils.printAsGitHubError(`The pull request number '${Deno.args[2].trim()}' is not a valid number.`);
+		Deno.exit(1);
+	}
 
-if (Utils.isNumeric(Deno.args[2].trim())) {
-	prNumber = parseInt(Deno.args[2].trim());
-} else {
-	Utils.printAsGitHubError(`The pull request number '${Deno.args[2].trim()}' is not a valid number.`);
-	Deno.exit(1);
-}
+	const headBranch = Deno.args[3].trim();
+	const expectedBranch = Deno.args[4].trim();
+	const label = Deno.args[5].trim();
+	const token = Deno.args[6].trim();
 
-const headBranch = Deno.args[3].trim();
-const expectedBranch = Deno.args[4].trim();
-const label = Deno.args[5].trim();
-const token = Deno.args[6].trim();
+	// Print out all of the arguments
+	Utils.printInGroup("Script Arguments", [
+		`Repo Owner (Required): ${ownerName}`,
+		`Repo Name (Required): ${repoName}`,
+		`Pull Request Number (Required): ${prNumber}`,
+		`Pull Request Head Branch (Required): ${headBranch}`,
+		`Expected Pull Request Head Branch (Required): ${expectedBranch}`,
+		`Label (Required): ${label}`,
+		"GitHub Token (Required): ****",
+	]);
 
-// Print out all of the arguments
-Utils.printInGroup("Script Arguments", [
-	`Repo Owner (Required): ${ownerName}`,
-	`Repo Name (Required): ${repoName}`,
-	`Pull Request Number (Required): ${prNumber}`,
-	`Pull Request Head Branch (Required): ${headBranch}`,
-	`Expected Pull Request Head Branch (Required): ${expectedBranch}`,
-	`Label (Required): ${label}`,
-	"GitHub Token (Required): ****",
-]);
+	const repoClient: RepoClient = new RepoClient(ownerName, repoName, token);
+	const repoDoesNotExist = !(await repoClient.exists());
 
-const repoClient: RepoClient = new RepoClient(ownerName, repoName, token);
-const repoDoesNotExist = !(await repoClient.exists());
+	if (repoDoesNotExist) {
+		Utils.printAsGitHubError(`The repository '${repoName}' does not exist.`);
+		Deno.exit(1);
+	}
 
-if (repoDoesNotExist) {
-	Utils.printAsGitHubError(`The repository '${repoName}' does not exist.`);
-	Deno.exit(1);
-}
+	// If the pull request head branch does not match the expected branch,
+	// do not add a label
+	if (headBranch != expectedBranch) {
+		Deno.exit(0);
+	}
 
-// If the pull request head branch does not match the expected branch,
-// do not add a label
-if (headBranch != expectedBranch) {
-	Deno.exit(0);
-}
+	const labelClient: LabelClient = new LabelClient(ownerName, repoName, token);
+	const labelDoesNotExist = !(await labelClient.labelExists(label));
 
-const labelClient: LabelClient = new LabelClient(ownerName, repoName, token);
-const labelDoesNotExist = !(await labelClient.labelExists(label));
+	if (labelDoesNotExist) {
+		Utils.printAsGitHubError(`The label '${label}' does not exist in the '${repoName}' repo.`);
+		Deno.exit(1);
+	}
 
-if (labelDoesNotExist) {
-	Utils.printAsGitHubError(`The label '${label}' does not exist in the '${repoName}' repo.`);
-	Deno.exit(1);
-}
+	const prClient: PullRequestClient = new PullRequestClient(ownerName, repoName, token);
+	await prClient.addLabel(prNumber, label);
+};
 
-const prClient: PullRequestClient = new PullRequestClient(ownerName, repoName, token);
-await prClient.addLabel(prNumber, label);
+labelIfHeadBranchExecutor();
+
+export default labelIfHeadBranchExecutor;
