8d1a7184342f4bcb3ad813827859e4bfe2c9b852
Calvin Wilkinson <kinsondigital@gmail.com>
2023-12-09 12:17:25 +0000

    deps: update kd_clients modules to latest version

--- deps.ts
+++ deps.ts
@@ -6,14 +6,14 @@ import { extname, basename, resolve } from "https://deno.land/std@0.207.0/path/m
 import {
 	IssueClient, PullRequestClient, ProjectClient, UsersClient, ReleaseClient,
 	OrgClient, RepoClient, TagClient, LabelClient, MilestoneClient, GitClient
-} from "https://deno.land/x/kd_clients@v1.0.0-preview.5/GitHubClients/mod.ts";
-import { XClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/OtherClients/mod.ts";
-import { NuGetClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/PackageClients/mod.ts";
+} from "https://deno.land/x/kd_clients@v1.0.0-preview.7/GitHubClients/mod.ts";
+import { XClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.7/OtherClients/mod.ts";
+import { NuGetClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.7/PackageClients/mod.ts";
 
 import {
 	IssueModel, LabelModel, ProjectModel, PullRequestModel, UserModel,
 	GitHubVarModel, MilestoneModel
-} from "https://deno.land/x/kd_clients@v1.0.0-preview.5/core/Models/mod.ts";
+} from "https://deno.land/x/kd_clients@v1.0.0-preview.7/core/Models/mod.ts";
 
 // CLIFFY
 import { Input } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/input.ts";
