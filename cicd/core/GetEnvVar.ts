52add4980bb7d7f130b3550ac64e19a2b9a401b2
CalvinWilkinson <kinsondigital@gmail.com>
2024-09-21 12:20:41 +0100

    chore: create function to validate and get env vars

---
+++ cicd/core/GetEnvVar.ts
@@ -0,0 +1,21 @@
+import { Utils } from "./Utils.ts";
+
+/**
+ * Get the value of an environment variable after checking if it exists.
+ * @param name The name of the environment variable.
+ * @remarks This function will throw an error if the environment variable does not exist.
+ */
+const getEnvVar = (name: string, scriptFileName?: string, throwErrorIfMissing: boolean = true): string => {
+	const value = (Deno.env.get(name) ?? "").trim();
+	
+	if (Utils.isNothing(value) && throwErrorIfMissing) {
+		const fileName = Utils.isNothing(scriptFileName) ? "" : `\n\t${scriptFileName}`;
+		const errorMsg = `The 'OWNER_NAME' environment variable does not exist.${fileName}`;
+		Utils.printAsGitHubError(errorMsg);
+		Deno.exit(1);
+	}
+
+	return value;
+}
+
+export default getEnvVar;
