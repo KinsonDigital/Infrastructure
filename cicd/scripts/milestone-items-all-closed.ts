ca87e5a88abf22ce3ecbf7d74017e055138ef7a9
CalvinWilkinson <kinsondigital@gmail.com>
2024-10-08 23:40:23 +0100

    chore: convert milestone check script to use env vars

--- cicd/scripts/milestone-items-all-closed.ts
+++ cicd/scripts/milestone-items-all-closed.ts
@@ -1,82 +1,59 @@
 import { MilestoneClient, RepoClient } from "../../deps.ts";
 import { IssueModel, PullRequestModel } from "../../deps.ts";
+import getEnvVar from "../core/GetEnvVar.ts";
 import { Utils } from "../core/Utils.ts";
 
-const milestoneItemsAllClosedExecutor = async () => {
-	if (Deno.args.length != 4) {
-		let errorMsg = `The cicd script must have at 4 arguments but has ${Deno.args.length} argument(s).`;
-		errorMsg += "\nThe 1st arg is required and must be the GitHub repository owner name.";
-		errorMsg += "\nThe 2nd arg is required and must be the GitHub repo name.";
-		errorMsg += "\nThe 3rd arg is required and must be the title of the milestone.";
-		errorMsg += "\nThe 4th arg is required and must be a GitHub PAT (Personal Access Token).";
+const scriptFileName = new URL(import.meta.url).pathname.split("/").pop();
 
-		Utils.printAsGitHubError(errorMsg);
-		Deno.exit(1);
+const ownerName = getEnvVar("OWNER_NAME", scriptFileName);
+const repoName = getEnvVar("REPO_NAME", scriptFileName);
+const milestoneTitle = getEnvVar("MILESTONE_TITLE", scriptFileName);
+const token = getEnvVar("GITHUB_TOKEN", scriptFileName);
+
+const repoClient: RepoClient = new RepoClient(ownerName, repoName, token);
+const repoDoesNotExist = !(await repoClient.exists());
+
+if (repoDoesNotExist) {
+	Utils.printAsGitHubError(`The repository '${repoName}' does not exist.`);
+	Deno.exit(1);
+}
+
+const milestoneClient: MilestoneClient = new MilestoneClient(ownerName, repoName, token);
+const milestoneItems = await milestoneClient.getIssuesAndPullRequests(milestoneTitle);
+
+const issues: IssueModel[] = Utils.filterIssues(milestoneItems);
+const prs: PullRequestModel[] = Utils.filterPullRequests(milestoneItems);
+
+const problemsFound: string[] = [];
+
+// Find all issues that are not closed
+issues.forEach((issue) => {
+	if (issue.state != "closed") {
+		problemsFound.push(`Issue #${issue.number} is not closed.\n\t${issue.html_url}`);
 	}
+});
 
-	// TODO: Fix all of these args
-	const ownerName = Deno.args[0].trim();
-	const repoName = Deno.args[1].trim();
-	const milestoneTitle = Deno.args[2].trim();
-	const token = Deno.args[3].trim();
-
-	// Print out all of the arguments
-	Utils.printInGroup("Script Arguments", [
-		`Owner Name (Required): ${ownerName}`,
-		`Repo Name (Required): ${repoName}`,
-		`Milestone Title (Required): ${milestoneTitle}`,
-		`GitHub Token (Required): ****`,
-	]);
-
-	const repoClient: RepoClient = new RepoClient(ownerName, repoName, token);
-	const repoDoesNotExist = !(await repoClient.exists());
-
-	if (repoDoesNotExist) {
-		Utils.printAsGitHubError(`The repository '${repoName}' does not exist.`);
-		Deno.exit(1);
+// Find all pull requests that are not closed
+prs.forEach((pr) => {
+	if (pr.state != "closed") {
+		problemsFound.push(`Pull request #${pr.number} is not closed.\n\t${pr.html_url}`);
 	}
+});
 
-	const milestoneClient: MilestoneClient = new MilestoneClient(ownerName, repoName, token);
-	const milestoneItems = await milestoneClient.getIssuesAndPullRequests(milestoneTitle);
-
-	const issues: IssueModel[] = Utils.filterIssues(milestoneItems);
-	const prs: PullRequestModel[] = Utils.filterPullRequests(milestoneItems);
-
-	const problemsFound: string[] = [];
-
-	// Find all issues that are not closed
-	issues.forEach((issue) => {
-		if (issue.state != "closed") {
-			problemsFound.push(`Issue #${issue.number} is not closed.\n\t${issue.html_url}`);
-		}
-	});
-
-	// Find all pull requests that are not closed
-	prs.forEach((pr) => {
-		if (pr.state != "closed") {
-			problemsFound.push(`Pull request #${pr.number} is not closed.\n\t${pr.html_url}`);
-		}
-	});
-
-	// Find all pull requests that are still in draft
-	prs.forEach((pr) => {
-		if (pr.draft === true) {
-			problemsFound.push(`Pull request #${pr.number} is still in draft.\n${pr.html_url}`);
-		}
-	});
-
-	let successMsg = `✅All issues and pull requests in milestone '${milestoneTitle}' are`;
-	successMsg += " closed and no pull requests are in draft.✅";
-
-	const failureMsg = `❌Something went wrong with closing all of the issues for milestone '${milestoneTitle}'.❌`;
-
-	Utils.printProblemList(problemsFound, successMsg, failureMsg);
-
-	if (problemsFound.length > 0) {
-		Deno.exit(1);
+// Find all pull requests that are still in draft
+prs.forEach((pr) => {
+	if (pr.draft === true) {
+		problemsFound.push(`Pull request #${pr.number} is still in draft.\n${pr.html_url}`);
 	}
-};
+});
 
-milestoneItemsAllClosedExecutor();
+let successMsg = `✅All issues and pull requests in milestone '${milestoneTitle}' are`;
+successMsg += " closed and no pull requests are in draft.✅";
 
-export default milestoneItemsAllClosedExecutor;
+const failureMsg = `❌Something went wrong with closing all of the issues for milestone '${milestoneTitle}'.❌`;
+
+Utils.printProblemList(problemsFound, successMsg, failureMsg);
+
+if (problemsFound.length > 0) {
+	Deno.exit(1);
+}
