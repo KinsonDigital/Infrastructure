43221eda6836f929be9c373a00873dd7c0f71bef
CalvinWilkinson <kinsondigital@gmail.com>
2024-10-09 00:06:14 +0100

    chore: update label if head branch script to env vars

--- cicd/scripts/mod.ts
+++ cicd/scripts/mod.ts
@@ -1,5 +1,4 @@
 // deno-lint-ignore-file no-unused-vars
 
-import labelIfHeadBranchExecutor from "./label-if-head-branch.ts";
 import prepareReleaseExecutor from "./prepare-release.ts";
 import syncPrToIssueExecutor from "./sync-pr-to-issue.ts";
