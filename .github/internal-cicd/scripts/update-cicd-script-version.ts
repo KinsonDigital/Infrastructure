25c31108a2684752db70bc45a15fd6d2fee6af56
Calvin Wilkinson <kinsondigital@gmail.com>
2025-02-09 14:55:28 +0000

    🚧Add auto update scripts (#255)
    * Start work for issue #254
    
    * cleanup: remove unused script
    
    * ci: create script to update repo variable
    
    * ci: add step to release workflow to update cicd scripts repo variable
    
    * ide: create launch config for update-cicd-script-version.ts script
    
    * deps: update deno lock
    
    * ci: convert vscode task script to cicd script
    
    * ci: add step to update reusable workflow versions
    
    * chore: remove unused exports
    
    * deps: update deno lock
    
    * ide: remove task
    
    * ci: update launch config
    
    * ide: add word to dictionary
    
    * ci: remove branch check step
    
    * ci: remove reusable workflow version check step
    
    * ci: fix spelling mistake
    
    * cleanup: remove unused imports
    
    * ci: add deno permissions
    
    * ci: fix spelling mistakes
    
    * ide: adjust deno permissions for launch config
    
    * ide: add words to dictionary
    
    * ci: update release workflow
    
    * ci: remove workflow version status check and associated scripts
    
    * ci: update step names
    
    * ci: update all action versions
    
    * ci: improve step names
    
    * ci: replace yarn with pnpm

---
+++ .github/internal-cicd/scripts/update-cicd-script-version.ts
@@ -0,0 +1,33 @@
+import { RepoClient } from "jsr:@kinsondigital/kd-clients@1.0.0-preview.14";
+import getEnvVar from "../../../cicd/core/GetEnvVar.ts";
+import { Utils } from "../../../cicd/core/Utils.ts";
+
+const scriptFileName = new URL(import.meta.url).pathname.split("/").pop();
+const ownerName = getEnvVar("OWNER_NAME", scriptFileName);
+const repoName = getEnvVar("REPO_NAME", scriptFileName);
+const token = getEnvVar("GITHUB_TOKEN", scriptFileName);
+const cicdScriptVersionRepoVarName = getEnvVar("CICD_SCRIPTS_VERSION_REPO_VAR_NAME", scriptFileName);
+const newVersion = getEnvVar("VERSION", scriptFileName);
+
+try {
+	const client = new RepoClient(ownerName, repoName, token);
+	const exists = await client.variableExists(cicdScriptVersionRepoVarName);
+
+	if (!exists) {
+		console.error(`The repository variable '${cicdScriptVersionRepoVarName}' does not exist.`);
+		Deno.exit(1);
+	}
+
+	await client.updateVariable(cicdScriptVersionRepoVarName, newVersion);
+
+	Utils.printAsGitHubNotice(`Updated the repository variable '${cicdScriptVersionRepoVarName}' value to '${newVersion}'.`);
+} catch (error) {
+	if (error instanceof Error) {
+		console.error(error.message);
+	} else {
+		console.error("An error occurred.");
+		console.error(error);
+	}
+
+	Deno.exit(1);	
+}
