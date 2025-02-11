6074a23f932d985ebec05dc81f98ee5394ee8792
CalvinWilkinson <kinsondigital@gmail.com>
2025-02-11 16:32:18 +0000

    ci: create script to update copyright in csproj files

---
+++ cicd/scripts/update-copyright.ts
@@ -0,0 +1,57 @@
+import { existsSync, walkSync } from "@std/fs";
+import getEnvVar from "../core/GetEnvVar.ts";
+import { Utils } from "../core/Utils.ts";
+
+const scriptFileName = new URL(import.meta.url).pathname.split("/").pop();
+const csProjFileName = getEnvVar("CS_PROJ_FILE_NAME", scriptFileName);
+
+const fileEntries = Array.from(walkSync(Deno.cwd(), {
+	includeDirs: false,
+	includeFiles: true,
+	skip: [/\.git/, /\.vscode/, /bin/, /obj/],
+	exts: [".csproj"],
+}));
+
+const foundEntries = fileEntries.find((entry) => entry.name === csProjFileName);
+
+if (foundEntries === undefined) {
+	Utils.printAsGitHubError(`The file ${csProjFileName} does not exist.`);
+	Deno.exit(1);
+}
+
+const csProjFilePath = foundEntries.path;
+
+if (!existsSync(csProjFilePath)) {
+	Utils.printAsGitHubError(`The file ${csProjFilePath} does not exist.`);
+	Deno.exit(1);
+}
+
+const fileContent = Deno.readTextFileSync(csProjFilePath);
+
+if (fileContent === undefined || fileContent === null || fileContent === "") {
+	Utils.printAsGitHubError("The file content is empty.");
+	Deno.exit(1);
+}
+
+const copyRightRegex = /<Copyright>[\s\S]*?<\/Copyright>/gm;
+
+const matchesResults = fileContent.match(copyRightRegex);
+
+if (matchesResults === null) {
+	Utils.printAsGitHubWarning("No matches found. This might be intentional.");
+	Deno.exit(0);
+}
+
+const matches = Array.from(matchesResults);
+
+if (matches.length <= 0) {
+	Utils.printAsGitHubWarning("No matches found. This might be intentional.");
+	Deno.exit(0);
+}
+
+const newTag = `<Copyright>Copyright ©${new Date().getFullYear()} Kinson Digital</Copyright>`;
+const newFileContent = fileContent.replace(copyRightRegex, newTag);
+
+Deno.writeTextFileSync(csProjFilePath, newFileContent);
+
+Utils.printAsGitHubNotice(`The csharp project file ${csProjFilePath} copyright has been updated.`);
