412e75b494591d0bf9d0a5c4c6fd70b8b15850ab
CalvinWilkinson <kinsondigital@gmail.com>
2025-10-13 21:14:40 +0100

    feat: create custom action to send a release announcement on X

--- actions/send-x-release-announcement/send-x-release-announcement.ts
+++ actions/send-x-release-announcement/send-x-release-announcement.ts
@@ -1,14 +1,10 @@
 import { XClient } from "jsr:@kinsondigital/kd-clients@1.0.0-preview.15/social";
-import { ReleaseXPostBuilder } from "../core/ReleaseXPostBuilder.ts";
-import { GitHubVariableService } from "../core/Services/GitHubVariableService.ts";
-import {
-getEnvVar,
-	isNothing,
-	isNotValidPreviewVersion,
-	isNotValidProdVersion,
-} from "../core/Utils.ts";
-import { validateOrgExists, validateRepoExists } from "../core/Validators.ts";
-import { printAsGitHubError, printAsGitHubNotice } from "../core/github.ts";
+import { RepoClient } from "jsr:@kinsondigital/kd-clients@1.0.0-preview.15/github";
+import { getEnvVar, isNotValidPreviewVersion, isNotValidProdVersion } from "../../cicd/core/Utils.ts";
+import { isNothing } from "../../cicd/core/guards.ts";
+import { GitHubVariableService } from "../../cicd/core/Services/GitHubVariableService.ts";
+import { validateOrgExists, validateRepoExists } from "../../cicd/core/Validators.ts";
+import { printAsGitHubError, printAsGitHubNotice } from "../../cicd/core/github.ts";
 
 // TODO: Need to import this from kd-clients library once it is available
 export interface XAuthValues {
@@ -42,15 +38,13 @@ const consumerAPIKey = getEnvVar("X_CONSUMER_API_KEY", scriptFileName);
 const consumerAPISecret = getEnvVar("X_CONSUMER_API_SECRET", scriptFileName);
 const accessTokenKey = getEnvVar("X_ACCESS_TOKEN_KEY", scriptFileName);
 const accessTokenSecret = getEnvVar("X_ACCESS_TOKEN_SECRET", scriptFileName);
-const githubToken = getEnvVar("GITHUB_TOKEN", scriptFileName);
-
-// TODO: update workflows to use these environment variables
 const X_BROADCAST_ENABLED = "X_BROADCAST_ENABLED";
 const xBroadcastEnabled = getEnvVar(X_BROADCAST_ENABLED, scriptFileName, false).toLowerCase();
 const templateRepoName = getEnvVar("RELEASE_X_POST_TEMPLATE_REPO_NAME", scriptFileName);
 const templateBranchName = getEnvVar("RELEASE_X_POST_TEMPLATE_BRANCH_NAME", scriptFileName);
 const relativeTemplateFilePath = getEnvVar("RELATIVE_RELEASE_X_POST_TEMPLATE_FILE_PATH", scriptFileName);
 const discordInviteCode = getEnvVar("DISCORD_INVITE_CODE", scriptFileName);
+const token = getEnvVar("GITHUB_TOKEN", scriptFileName);
 
 version = version.startsWith("v") ? version : `v${version}`;
 
@@ -64,7 +58,7 @@ if (isNotValidPreviewVersion(version) && isNotValidProdVersion(version)) {
 	Deno.exit(1);
 }
 
-const githubVarService = new GitHubVariableService(ownerName, repoName, githubToken);
+const githubVarService = new GitHubVariableService(ownerName, repoName, token);
 githubVarService.setOrgAndRepo(ownerName, repoName);
 
 if (isNothing(xBroadcastEnabled) || xBroadcastEnabled === "false") {
@@ -82,15 +76,23 @@ const authValues: XAuthValues = {
 	access_token_secret: accessTokenSecret,
 };
 
-const postBuilder: ReleaseXPostBuilder = new ReleaseXPostBuilder(ownerName, templateRepoName, githubToken);
+const repoClient = new RepoClient(ownerName, templateRepoName, token);
 
-const post = await postBuilder.buildPost(
-	templateBranchName,
-	relativeTemplateFilePath,
-	repoName,
-	version,
-	discordInviteCode,
-);
+const nugetVersion = version.startsWith("v") ? version.replace("v", "") : version;
+const templateDoesNotExist = !(await repoClient.fileExists(templateBranchName, relativeTemplateFilePath));
+
+if (templateDoesNotExist) {
+	printAsGitHubError(`The release X post template file '${relativeTemplateFilePath}' could not be found.`);
+	Deno.exit(1);
+}
+
+const templateFileData: string = await repoClient.getFileContent(templateBranchName, relativeTemplateFilePath);
+
+let post = templateFileData.replaceAll(`{PROJECT_NAME}`, repoName);
+post = post.replaceAll(`{$VERSION}`, version);
+post = post.replaceAll(`{$NUGET_VERSION_VAR}`, nugetVersion);
+post = post.replaceAll(`{$REPO_OWNER_VAR}`, ownerName);
+post = post.replaceAll(`{$DISCORD_INVITE_CODE_VAR}`, discordInviteCode);
 
 const xClient: XClient = new XClient(authValues);
 await xClient.tweet(post);
