92524c94c61303cad03ceee27bf4f66f449c18a1
CalvinWilkinson <kinsondigital@gmail.com>
2025-10-15 14:46:02 +0100

    deps: update jsr module and deno lock file

--- dev-tools/feat-pr-creator-tool.ts
+++ dev-tools/feat-pr-creator-tool.ts
@@ -1,4 +1,4 @@
-import { delay } from "jsr:@std/async@1.0.14";
+import { delay } from "jsr:@std/async@1.0.15";
 import { existsSync, walkSync } from "jsr:@std/fs@1.0.19";
 import { Input } from "jsr:@cliffy/prompt@1.0.0-rc.8/input";
 import { Select } from "jsr:@cliffy/prompt@1.0.0-rc.8/select";
