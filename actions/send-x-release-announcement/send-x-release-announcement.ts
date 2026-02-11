e3de323b64a630509f8f6bdc36a2b89a724c6746
Calvin Wilkinson <kinsondigital@gmail.com>
2026-02-11 23:08:22 +0000

    X announcement action improvements (#279)
    * Start work for issue #276
    
    * feat: add website url action input to be used in the template
    
    * enhance!: remove the repo-name action input
    
    * enhance!: rename the template repo name input and make it optional
    
    * enhance!: rename the template branch name input and make it optional
    
    * enhance!: rename repo temp relative file path input & make optional
    
    * feat: add a new input for loading a template file locally
    
    * enhance!: sets the discord invite code input as optional
    
    * chore: add simple improvements
    
    * ide: replace unused launch config with send x post launch config
    
    * chore: create test templates for testing send x release post action
    
    * deps: update deno lock

--- actions/send-x-release-announcement/send-x-release-announcement.ts
+++ actions/send-x-release-announcement/send-x-release-announcement.ts
@@ -1,8 +1,8 @@
 import { XClient } from "jsr:@kinsondigital/kd-clients@1.0.0-preview.15/social";
 import { RepoClient } from "jsr:@kinsondigital/kd-clients@1.0.0-preview.15/github";
+import { existsSync } from "jsr:@std/fs@1.0.22";
 import { getEnvVar, isNotValidPreviewVersion, isNotValidProdVersion } from "../../cicd/core/Utils.ts";
 import { isNothing } from "../../cicd/core/guards.ts";
-import { GitHubVariableService } from "../../cicd/core/Services/GitHubVariableService.ts";
 import { validateOrgExists, validateRepoExists } from "../../cicd/core/Validators.ts";
 import { printAsGitHubError, printAsGitHubNotice } from "../../cicd/core/github.ts";
 
@@ -32,24 +32,43 @@ export interface XAuthValues {
 const scriptFileName = new URL(import.meta.url).pathname.split("/").pop();
 
 const ownerName = getEnvVar("OWNER_NAME", scriptFileName);
-const repoName = getEnvVar("REPO_NAME", scriptFileName);
 let version = getEnvVar("VERSION", scriptFileName).toLowerCase();
+const websiteUrl = (Deno.env.get("WEBSITE_URL") || "").trim(); // Optional
 const consumerAPIKey = getEnvVar("X_CONSUMER_API_KEY", scriptFileName);
 const consumerAPISecret = getEnvVar("X_CONSUMER_API_SECRET", scriptFileName);
 const accessTokenKey = getEnvVar("X_ACCESS_TOKEN_KEY", scriptFileName);
 const accessTokenSecret = getEnvVar("X_ACCESS_TOKEN_SECRET", scriptFileName);
-const X_BROADCAST_ENABLED = "X_BROADCAST_ENABLED";
-const xBroadcastEnabled = getEnvVar(X_BROADCAST_ENABLED, scriptFileName, false).toLowerCase();
-const templateRepoName = getEnvVar("RELEASE_X_POST_TEMPLATE_REPO_NAME", scriptFileName);
-const templateBranchName = getEnvVar("RELEASE_X_POST_TEMPLATE_BRANCH_NAME", scriptFileName);
-const relativeTemplateFilePath = getEnvVar("RELATIVE_RELEASE_X_POST_TEMPLATE_FILE_PATH", scriptFileName);
-const discordInviteCode = getEnvVar("DISCORD_INVITE_CODE", scriptFileName);
+const xBroadcastEnabled = getEnvVar("X_BROADCAST_ENABLED", scriptFileName, false).toLowerCase();
+const templateRepoName = (Deno.env.get("POST_TEMPLATE_REPO_NAME") ?? "").trim(); // Optional
+const templateBranchName = (Deno.env.get("POST_TEMPLATE_BRANCH_NAME") ?? "").trim(); // Optional
+const relativeTemplateFilePath = (Deno.env.get("POST_TEMPLATE_REPO_RELATIVE_FILE_PATH") ?? "").trim(); // Optional
+const localPostTemplateFilePath = (Deno.env.get("LOCAL_POST_TEMPLATE_FILE_PATH") ?? "").trim(); // Optional
+const discordInviteCode = (Deno.env.get("DISCORD_INVITE_CODE") ?? "").trim(); // Optional
 const token = getEnvVar("GITHUB_TOKEN", scriptFileName);
 
 version = version.startsWith("v") ? version : `v${version}`;
 
+// Print info about the available template variables for use
+const tempVarsInfoMsg = `The following template variables are available for use in the release announcement post template:` +
+	"\n\tPROJECT_NAME" +
+	"\n\tVERSION" +
+	"\n\tWEBSITE_URL" +
+	"\n\tNUGET_VERSION_VAR" +
+	"\n\tREPO_OWNER_VAR" +
+	"\n\tDISCORD_INVITE_CODE_VAR";
+printAsGitHubNotice(tempVarsInfoMsg);
+
+// If the local post template file path is not provided, and the repository inputs are provided
+const getTemplateFromRemoteRepo = isNothing(localPostTemplateFilePath) &&
+	!isNothing(templateRepoName) && !isNothing(templateBranchName) && !isNothing(relativeTemplateFilePath);
+
+const getTemplateFromLocalFile = !isNothing(localPostTemplateFilePath);
+
 await validateOrgExists(ownerName, token);
-await validateRepoExists(ownerName, repoName, token);
+
+if (getTemplateFromRemoteRepo) {
+	await validateRepoExists(ownerName, templateRepoName, token);
+}
 
 if (isNotValidPreviewVersion(version) && isNotValidProdVersion(version)) {
 	let errorMsg = `The version '${version}' is not a valid preview or production version.`;
@@ -58,12 +77,9 @@ if (isNotValidPreviewVersion(version) && isNotValidProdVersion(version)) {
 	Deno.exit(1);
 }
 
-const githubVarService = new GitHubVariableService(ownerName, repoName, token);
-githubVarService.setOrgAndRepo(ownerName, repoName);
-
 if (isNothing(xBroadcastEnabled) || xBroadcastEnabled === "false") {
 	const noticeMsg = `No X post broadcast will be performed.` +
-		`\nTo enable X post broadcasting, set the '${X_BROADCAST_ENABLED}' variable to 'true'.` +
+		`\nTo enable X post broadcasting, set the 'X_BROADCAST_ENABLED' variable to 'true'.` +
 		"\nIf the variable is missing, empty, or set to 'false', no X post broadcast will be performed.";
 	printAsGitHubNotice(noticeMsg);
 	Deno.exit(0);
@@ -76,25 +92,53 @@ const authValues: XAuthValues = {
 	access_token_secret: accessTokenSecret,
 };
 
-const repoClient = new RepoClient(ownerName, templateRepoName, token);
+let templateFileData = "";
 
-const nugetVersion = version.startsWith("v") ? version.replace("v", "") : version;
-const templateDoesNotExist = !(await repoClient.fileExists(templateBranchName, relativeTemplateFilePath));
+if (getTemplateFromRemoteRepo) {
+	const repoClient = new RepoClient(ownerName, templateRepoName, token);
 
-if (templateDoesNotExist) {
-	printAsGitHubError(`The release X post template file '${relativeTemplateFilePath}' could not be found.`);
+	const templateDoesNotExist = !(await repoClient.fileExists(templateBranchName, relativeTemplateFilePath));
+
+	if (templateDoesNotExist) {
+		printAsGitHubError(`The release X post template file '${relativeTemplateFilePath}' could not be found.`);
+		Deno.exit(1);
+	}
+
+	templateFileData = await repoClient.getFileContent(templateBranchName, relativeTemplateFilePath);
+} else if (getTemplateFromLocalFile) {
+	// Check if the local file does not exist
+	if (!existsSync(localPostTemplateFilePath)) {
+		const errorMsg = `The local release X post template file '${localPostTemplateFilePath}' could not be found.` +
+			"\nPlease ensure that the repository is checked out, the file exists at the specified path, or provide valid repository template inputs.";
+		printAsGitHubError(errorMsg);
+		Deno.exit(1);
+	}
+
+	templateFileData = await Deno.readTextFile(localPostTemplateFilePath);
+} else {
+	const errorMsg = "Missing required inputs for the release announcement post template." +
+		"\nPlease provide either the 'local-post-template-file-path' input value or all of the " +
+		"('post-template-repo-name', 'post-template-branch-name', 'post-template-repo-relative-file-path') input values." +
+		"\nThe input 'local-post-template-file-path' input takes precedence over the repository template inputs.";
+	printAsGitHubError(errorMsg);
 	Deno.exit(1);
 }
 
-const templateFileData: string = await repoClient.getFileContent(templateBranchName, relativeTemplateFilePath);
+if (isNothing(templateFileData)) {
+	printAsGitHubError("The release announcement post template file is empty.");
+	Deno.exit(1);
+}
 
-let post = templateFileData.replaceAll(`{PROJECT_NAME}`, repoName);
-post = post.replaceAll(`{$VERSION}`, version);
-post = post.replaceAll(`{$NUGET_VERSION_VAR}`, nugetVersion);
-post = post.replaceAll(`{$REPO_OWNER_VAR}`, ownerName);
-post = post.replaceAll(`{$DISCORD_INVITE_CODE_VAR}`, discordInviteCode);
+const nugetVersion = version.startsWith("v") ? version.replace("v", "") : version;
+
+let post = templateFileData.replaceAll(`{PROJECT_NAME}`, templateRepoName);
+post = post.replaceAll(`{VERSION}`, version);
+post = post.replaceAll("{WEBSITE_URL}", websiteUrl);
+post = post.replaceAll(`{NUGET_VERSION_VAR}`, nugetVersion);
+post = post.replaceAll(`{REPO_OWNER_VAR}`, ownerName);
+post = post.replaceAll(`{DISCORD_INVITE_CODE_VAR}`, discordInviteCode);
 
 const xClient: XClient = new XClient(authValues);
 await xClient.tweet(post);
 
-printAsGitHubNotice(`A release X post was successfully broadcasted for the '${repoName}' project for version '${version}'.`);
+printAsGitHubNotice(`A release X post was successfully broadcasted for the '${templateRepoName}' project for version '${version}'.`);
