9fa8c37fb3a4b7dab6efc99b4d055957dcc7338d
CalvinWilkinson <kinsondigital@gmail.com>
2026-02-12 17:39:51 +0000

    fix: fix various issues with the x release announcement action

--- actions/send-x-release-announcement/send-x-release-announcement.ts
+++ actions/send-x-release-announcement/send-x-release-announcement.ts
@@ -32,13 +32,13 @@ export interface XAuthValues {
 const scriptFileName = new URL(import.meta.url).pathname.split("/").pop();
 
 const ownerName = getEnvVar("OWNER_NAME", scriptFileName);
+const projectName = (Deno.env.get("PROJECT_NAME") ?? "").trim();
 let version = getEnvVar("VERSION", scriptFileName).toLowerCase();
 const websiteUrl = (Deno.env.get("WEBSITE_URL") || "").trim(); // Optional
-const consumerAPIKey = getEnvVar("X_CONSUMER_API_KEY", scriptFileName);
-const consumerAPISecret = getEnvVar("X_CONSUMER_API_SECRET", scriptFileName);
-const accessTokenKey = getEnvVar("X_ACCESS_TOKEN_KEY", scriptFileName);
-const accessTokenSecret = getEnvVar("X_ACCESS_TOKEN_SECRET", scriptFileName);
-const xBroadcastEnabled = getEnvVar("X_BROADCAST_ENABLED", scriptFileName, false).toLowerCase();
+const consumerAPIKey = getEnvVar("CONSUMER_API_KEY", scriptFileName);
+const consumerAPISecret = getEnvVar("CONSUMER_API_SECRET", scriptFileName);
+const accessTokenKey = getEnvVar("ACCESS_TOKEN_KEY", scriptFileName);
+const accessTokenSecret = getEnvVar("ACCESS_TOKEN_SECRET", scriptFileName);
 const templateRepoName = (Deno.env.get("POST_TEMPLATE_REPO_NAME") ?? "").trim(); // Optional
 const templateBranchName = (Deno.env.get("POST_TEMPLATE_BRANCH_NAME") ?? "").trim(); // Optional
 const relativeTemplateFilePath = (Deno.env.get("POST_TEMPLATE_REPO_RELATIVE_FILE_PATH") ?? "").trim(); // Optional
@@ -77,14 +77,6 @@ if (isNotValidPreviewVersion(version) && isNotValidProdVersion(version)) {
 	Deno.exit(1);
 }
 
-if (isNothing(xBroadcastEnabled) || xBroadcastEnabled === "false") {
-	const noticeMsg = `No X post broadcast will be performed.` +
-		`\nTo enable X post broadcasting, set the 'X_BROADCAST_ENABLED' variable to 'true'.` +
-		"\nIf the variable is missing, empty, or set to 'false', no X post broadcast will be performed.";
-	printAsGitHubNotice(noticeMsg);
-	Deno.exit(0);
-}
-
 const authValues: XAuthValues = {
 	consumer_api_key: consumerAPIKey,
 	consumer_api_secret: consumerAPISecret,
@@ -119,7 +111,7 @@ if (getTemplateFromRemoteRepo) {
 	const errorMsg = "Missing required inputs for the release announcement post template." +
 		"\nPlease provide either the 'local-post-template-file-path' input value or all of the " +
 		"('post-template-repo-name', 'post-template-branch-name', 'post-template-repo-relative-file-path') input values." +
-		"\nThe input 'local-post-template-file-path' input takes precedence over the repository template inputs.";
+		"\nThe 'local-post-template-file-path' input takes precedence over the repository template inputs.";
 	printAsGitHubError(errorMsg);
 	Deno.exit(1);
 }
@@ -131,14 +123,14 @@ if (isNothing(templateFileData)) {
 
 const nugetVersion = version.startsWith("v") ? version.replace("v", "") : version;
 
-let post = templateFileData.replaceAll(`{PROJECT_NAME}`, templateRepoName);
-post = post.replaceAll(`{VERSION}`, version);
-post = post.replaceAll("{WEBSITE_URL}", websiteUrl);
-post = post.replaceAll(`{NUGET_VERSION_VAR}`, nugetVersion);
-post = post.replaceAll(`{REPO_OWNER_VAR}`, ownerName);
-post = post.replaceAll(`{DISCORD_INVITE_CODE_VAR}`, discordInviteCode);
+const post = templateFileData.replaceAll(`{PROJECT_NAME}`, projectName)
+	.replaceAll(`{VERSION}`, version)
+	.replaceAll("{WEBSITE_URL}", websiteUrl)
+	.replaceAll(`{NUGET_VERSION_VAR}`, nugetVersion)
+	.replaceAll(`{REPO_OWNER_VAR}`, ownerName)
+	.replaceAll(`{DISCORD_INVITE_CODE_VAR}`, discordInviteCode);
 
 const xClient: XClient = new XClient(authValues);
 await xClient.tweet(post);
 
-printAsGitHubNotice(`A release X post was successfully broadcasted for the '${templateRepoName}' project for version '${version}'.`);
+printAsGitHubNotice(`A release X post was successfully posted for the '${projectName}' project for version '${version}'.`);
