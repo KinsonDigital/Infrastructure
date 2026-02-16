c8d5cc2b1718838eeb14a0f24577ffdf769fbe8b
Calvin Wilkinosn <kinsondigital@gmail.com>
2026-02-16 16:30:33 +0000

    feat: create custom action to post bluesky release announcements

---
+++ actions/bluesky-release-announcement/bluesky-release-announcement.ts
@@ -0,0 +1,159 @@
+import { AppBskyFeedPost, AtpAgent, RichText } from "npm:@atproto/api@0.18.21";
+import { RepoClient } from "jsr:@kinsondigital/kd-clients@1.0.0-preview.15/github";
+import { existsSync } from "jsr:@std/fs@1.0.22";
+import { getEnvVar, isNotValidPreviewVersion, isNotValidProdVersion } from "../../cicd/core/Utils.ts";
+import { isNothing } from "../../cicd/core/guards.ts";
+import { validateOrgExists, validateRepoExists } from "../../cicd/core/Validators.ts";
+import { printAsGitHubError, printAsGitHubNotice } from "../../cicd/core/github.ts";
+
+// TODO: Need to import this from kd-clients library once it is available
+export interface XAuthValues {
+	/**
+	 * Gets or sets the consumer key.
+	 */
+	consumer_api_key: string;
+
+	/**
+	 * Gets or sets the consumer secret.
+	 */
+	consumer_api_secret: string;
+
+	/**
+	 * Gets or sets the access token key.
+	 */
+	access_token_key: string;
+
+	/**
+	 * Gets or sets the access token secret.
+	 */
+	access_token_secret: string;
+}
+
+const scriptFileName = new URL(import.meta.url).pathname.split("/").pop();
+
+const ownerName = getEnvVar("OWNER_NAME", scriptFileName);
+const projectName = (Deno.env.get("PROJECT_NAME") ?? "").trim();
+let version = getEnvVar("VERSION", scriptFileName).toLowerCase();
+const websiteUrl = (Deno.env.get("WEBSITE_URL") || "").trim(); // Optional
+const templateRepoName = (Deno.env.get("POST_TEMPLATE_REPO_NAME") ?? "").trim(); // Optional
+const templateBranchName = (Deno.env.get("POST_TEMPLATE_BRANCH_NAME") ?? "").trim(); // Optional
+const relativeTemplateFilePath = (Deno.env.get("POST_TEMPLATE_REPO_RELATIVE_FILE_PATH") ?? "").trim(); // Optional
+const localPostTemplateFilePath = (Deno.env.get("LOCAL_POST_TEMPLATE_FILE_PATH") ?? "").trim(); // Optional
+const service = getEnvVar("SERVICE", scriptFileName);
+const identifier = getEnvVar("IDENTIFIER", scriptFileName);
+const password = getEnvVar("PASSWORD", scriptFileName);
+const discordInviteCode = (Deno.env.get("DISCORD_INVITE_CODE") ?? "").trim(); // Optional
+const token = getEnvVar("GITHUB_TOKEN", scriptFileName);
+
+version = version.startsWith("v") ? version : `v${version}`;
+
+// Print info about the available template variables for use
+const tempVarsInfoMsg = `The following template variables are available for use in the release announcement post template:` +
+	"\n\tPROJECT_NAME" +
+	"\n\tVERSION" +
+	"\n\tWEBSITE_URL" +
+	"\n\tNUGET_VERSION_VAR" +
+	"\n\tREPO_OWNER_VAR" +
+	"\n\tDISCORD_INVITE_CODE_VAR";
+printAsGitHubNotice(tempVarsInfoMsg);
+
+// If the local post template file path is not provided, and the repository inputs are provided
+const getTemplateFromRemoteRepo = isNothing(localPostTemplateFilePath) &&
+	!isNothing(templateRepoName) && !isNothing(templateBranchName) && !isNothing(relativeTemplateFilePath);
+
+const getTemplateFromLocalFile = !isNothing(localPostTemplateFilePath);
+
+await validateOrgExists(ownerName, token);
+
+if (getTemplateFromRemoteRepo) {
+	await validateRepoExists(ownerName, templateRepoName, token);
+}
+
+if (isNotValidPreviewVersion(version) && isNotValidProdVersion(version)) {
+	let errorMsg = `The version '${version}' is not a valid preview or production version.`;
+	errorMsg += "\nRequired Syntax: v#.#.# or v#.#.#-preview.#";
+	printAsGitHubError(errorMsg);
+	Deno.exit(1);
+}
+
+let templateFileData = "";
+
+if (getTemplateFromRemoteRepo) {
+	const repoClient = new RepoClient(ownerName, templateRepoName, token);
+
+	const templateDoesNotExist = !(await repoClient.fileExists(templateBranchName, relativeTemplateFilePath));
+
+	if (templateDoesNotExist) {
+		printAsGitHubError(`The release Bluesky post template file '${relativeTemplateFilePath}' could not be found.`);
+		Deno.exit(1);
+	}
+
+	templateFileData = await repoClient.getFileContent(templateBranchName, relativeTemplateFilePath);
+} else if (getTemplateFromLocalFile) {
+	// Check if the local file does not exist
+	if (!existsSync(localPostTemplateFilePath)) {
+		const errorMsg = `The local release Bluesky post template file '${localPostTemplateFilePath}' could not be found.` +
+			"\nPlease ensure that the repository is checked out, the file exists at the specified path, or provide valid repository template inputs.";
+		printAsGitHubError(errorMsg);
+		Deno.exit(1);
+	}
+
+	templateFileData = await Deno.readTextFile(localPostTemplateFilePath);
+} else {
+	const errorMsg = "Missing required inputs for the release announcement post template." +
+		"\nPlease provide either the 'local-post-template-file-path' input value or all of the " +
+		"('post-template-repo-name', 'post-template-branch-name', 'post-template-repo-relative-file-path') input values." +
+		"\nThe 'local-post-template-file-path' input takes precedence over the repository template inputs.";
+	printAsGitHubError(errorMsg);
+	Deno.exit(1);
+}
+
+if (isNothing(templateFileData)) {
+	printAsGitHubError("The release announcement post template file is empty.");
+	Deno.exit(1);
+}
+
+const nugetVersion = version.startsWith("v") ? version.replace("v", "") : version;
+
+const post = templateFileData.replaceAll(`{PROJECT_NAME}`, projectName)
+	.replaceAll(`{VERSION}`, version)
+	.replaceAll("{WEBSITE_URL}", websiteUrl)
+	.replaceAll(`{NUGET_VERSION}`, nugetVersion)
+	.replaceAll(`{REPO_OWNER}`, ownerName)
+	.replaceAll(`{DISCORD_INVITE_CODE}`, discordInviteCode);
+
+console.log(post);
+
+const agent = new AtpAgent({ service });
+
+await agent.login({ identifier, password });
+
+const richText = new RichText({
+	text: post,
+});
+
+await richText.detectFacets(agent);
+
+if (richText.graphemeLength > 300) {
+	printAsGitHubError(
+		`The length of the release announcement post (${richText.graphemeLength} graphemes) exceeds the maximum allowed length of 300 graphemes for a Bluesky post.` +
+			`\nPlease shorten the post content to fit within the limit. The post content after template variable replacement is:\n${post}`,
+	);
+
+	Deno.exit(1);
+}
+
+const postRecord: AppBskyFeedPost.Record = {
+	$type: "app.bsky.feed.post",
+	text: richText.text,
+	facets: richText.facets,
+	createdAt: new Date().toISOString(),
+};
+
+const postResult = await agent.post(postRecord);
+
+printAsGitHubNotice(
+	`A release Bluesky post was successfully broadcasted for the '${projectName}' project for version '${version}'.`,
+);
+printAsGitHubNotice(`\tPost URI: ${postResult.uri}`); // Indicates the repository DID, collection, and record key
+printAsGitHubNotice(`\tPost CID: ${postResult.cid}`); // A hash of the record itself
---
+++ cicd/core/text.ts
@@ -0,0 +1,29 @@
+/**
+ * Calculates the byte positions of a specified occurrence of a substring within a given text.
+ * @param text The text that contains the substring for which to calculate byte positions.
+ * @param substring The substring for which to calculate byte positions within the text.
+ * @param occurrence The occurrence of the substring.
+ * @returns
+ */
+export function getBytePosition(
+	text: string,
+	substring: string,
+	occurrence = 1,
+): { byteStart: number; byteEnd: number } | null {
+	const encoder = new TextEncoder();
+
+	let index = -1;
+
+	for (let i = 0; i < occurrence; i++) {
+		index = text.indexOf(substring, index + 1);
+
+		if (index === -1) {
+			return null;
+		}
+	}
+
+	const byteStart = encoder.encode(text.slice(0, index)).length;
+	const byteEnd = byteStart + encoder.encode(substring).length;
+
+	return { byteStart, byteEnd };
+}
