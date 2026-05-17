6b33f99ba260e25786ffc18d62eed17ef732a527
CalvinWilkinson <kinsondigital@gmail.com>
2026-05-17 10:13:17 +0100

    chore: improve new action

--- actions/rename-milestone/rename-milestone.ts
+++ actions/rename-milestone/rename-milestone.ts
@@ -3,13 +3,21 @@ import { getEnvVar } from "../../cicd/core/Utils.ts";
 
 const scriptFileName = new URL(import.meta.url).pathname.split("/").pop();
 
-const ownerName = getEnvVar("OWNER_NAME", scriptFileName);
+const ownerName = getEnvVar("REPO_OWNER", scriptFileName);
 const repoName = getEnvVar("REPO_NAME", scriptFileName);
-const githubToken = getEnvVar("EA_CICD_TOKEN", scriptFileName);
+const githubToken = getEnvVar("GITHUB_TOKEN", scriptFileName);
 const currentMilestoneName = getEnvVar("CURRENT_MILESTONE_NAME", scriptFileName);
 const newMilestoneName = getEnvVar("NEW_MILESTONE_NAME", scriptFileName);
 
 const milestoneClient = new MilestoneClient(ownerName, repoName, githubToken);
+
+const exists = await milestoneClient.exists(currentMilestoneName);
+
+if (!exists) {
+	console.error(`Milestone with name "${currentMilestoneName}" does not exist.`);
+	Deno.exit(1);
+}
+
 const milestone = await milestoneClient.getMilestoneByName(currentMilestoneName);
 
 const url = `https://api.github.com/repos/${ownerName}/${repoName}/milestones/${milestone.number}`;
@@ -26,6 +34,9 @@ const response = await fetch(url, {
 });
 
 if (!response.ok) {
-	console.error(`Error: ${response.status} ${response.statusText}`);
+	const responseBody = (await response.text()).trim();
+	const details = responseBody.length > 0 ? ` - ${responseBody}` : "";
+
+	console.error(`Error renaming milestone: ${response.status} ${response.statusText}${details}`);
 	Deno.exit(1);
 }
